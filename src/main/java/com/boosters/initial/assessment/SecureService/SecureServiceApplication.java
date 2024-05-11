0b0916d4535e84d330282af313ec984b53e9799f
Antonio Casillas <casillas51@gmail.com>
2024-05-10 22:24:14 -0600

    Feature. Initial version
    Add initial version with next features:
    * Add compose to init database on docker
    * Add Development and Test profiles
    * Create start class to run spring boot app

---
+++ src/main/java/com/boosters/initial/assessment/SecureService/SecureServiceApplication.java
@@ -0,0 +1,14 @@
+package com.boosters.initial.assessment.SecureService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SecureServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boosters/initial/assessment/SecureService/SecureServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boosters.initial.assessment.SecureService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
